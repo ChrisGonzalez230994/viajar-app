d39bce735412f57b372b18d141a82a8be371aa0f
Christian Gonzalez <chris.gonzalez.alejandro@gmail.com>
2025-10-27 21:15:52 -0300

    2.1
    Arreglo del pruba

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,21 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Login } from './components/login/login';
-import { Resena } from './components/resena/resena';
 import { Registro } from './components/registro/registro';
 import { Home } from './components/pages/home/home';
 import { Navbar } from './components/navbar/navbar';
-import { Pruba } from './components/pruba/pruba';
 
 @NgModule({
   declarations: [
     App,
     Login,
-    Resena,
     Registro,
     Home,
     Navbar,
-    Pruba
   ],
   imports: [
     BrowserModule,
